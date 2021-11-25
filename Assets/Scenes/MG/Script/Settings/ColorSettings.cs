bc8222f23f6c9aed625e3da7b5c7238bf345f794
Manuel Guimaraes <manuel_guimaraes@hotmail.com>
2021-11-25 18:36:37 +0000

    Messing around with Mesh Renderers and Noise Filters.

--- Assets/Scenes/MG/Script/Settings/ColorSettings.cs
+++ Assets/Scenes/MG/Script/Settings/ColorSettings.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu]
+[CreateAssetMenu()]
 public class ColorSettings : ScriptableObject
 {
     public Color planetColor;
