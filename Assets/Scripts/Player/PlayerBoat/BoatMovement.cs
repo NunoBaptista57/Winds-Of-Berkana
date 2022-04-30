6aa58a02aca750d09b7a6172e06583503c0ed497
João Patrício <joaopat98@gmail.com>
2022-04-30 03:35:34 +0100

    Remove unused variables on movement, free camera

--- Assets/Scripts/Player/PlayerBoat/BoatMovement.cs
+++ Assets/Scripts/Player/PlayerBoat/BoatMovement.cs
@@ -8,36 +8,19 @@ using UnityEngine.InputSystem;
 public class BoatMovement : MonoBehaviour
 {
     new Rigidbody rigidbody;
-    [Header("Wind/Sails")]
+    [Header("Wind")]
     [Min(0), SerializeField] float WindForce;
-    [Min(0), SerializeField] float MinSailMultiplier = 0.0f;
-
-    [Min(0), SerializeField] float SailEffectStrength = 1;
-    [Min(0), SerializeField] float ReelSpeed = 1;
     [Min(0)] public float MaxVelocity;
     [ReadOnlyInspector] public float CurrentMaxVelocity;
     [Min(0), SerializeField] float VelocityLimitingStrength = 1;
     [Min(0), SerializeField] float TurningTorque;
-
-    [Header("Boost")]
-    [Min(0), SerializeField] float BoostMaxSpeedMultiplier = 1.1f;
-
-    [Min(0), SerializeField] float MaxBoostCapacity = 1f;
-    [Min(0), SerializeField] float BoostUsagePerSecond = 0.3f;
-    [Min(0), SerializeField] float BoostRecoveredPerSecond = 0.2f;
-    [ReadOnlyInspector, SerializeField] float CurrentBoost = 0;
-    [ReadOnlyInspector, SerializeField] bool Boosting = false;
-    [ReadOnlyInspector, SerializeField] bool BoostRecharging = false;
 
     struct PlayerInput
     {
         public float Turn;
         public float Pitch;
         public float Reel;
-        public bool Boost;
     }
-    [ReadOnlyInspector, SerializeField] float currentSail;
-    [ReadOnlyInspector, SerializeField] float currentSailMultiplier;
 
     [Header("Angle Limiting")]
     [SerializeField, Range(0, 90)] float MinVerticalAngle;
@@ -56,49 +39,12 @@ public class BoatMovement : MonoBehaviour
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
-        currentSail = 1;
-        currentSailMultiplier = 1;
         CurrentMaxVelocity = MaxVelocity;
-
-        CurrentBoost = MaxBoostCapacity;
-        BoostRecharging = false;
     }
 
     void Update()
     {
-        currentSail = Mathf.Clamp(currentSail + input.Reel * Time.deltaTime, 0, 1);
-        currentSailMultiplier = Mathf.Lerp(
-            currentSailMultiplier,
-            Mathf.Lerp(MinSailMultiplier, 1, currentSail),
-            SailEffectStrength * Time.deltaTime);
 
-        if (input.Boost && !BoostRecharging)
-        {
-            Boosting = true;
-            CurrentBoost -= BoostUsagePerSecond * Time.deltaTime;
-            if (CurrentBoost <= 0)
-            {
-                CurrentBoost = 0;
-                Boosting = false;
-                BoostRecharging = true;
-            }
-        }
-        else if (CurrentBoost > MaxBoostCapacity)
-        {
-            CurrentBoost = MaxBoostCapacity;
-            BoostRecharging = false;
-            Boosting = false;
-        }
-        else if (CurrentBoost < MaxBoostCapacity)
-        {
-            CurrentBoost += BoostRecoveredPerSecond * Time.deltaTime;
-            Boosting = false;
-            if (CurrentBoost > MaxBoostCapacity)
-            {
-                CurrentBoost = MaxBoostCapacity;
-                BoostRecharging = false;
-            }
-        }
     }
 
     void FixedUpdate()
@@ -106,9 +52,9 @@ public class BoatMovement : MonoBehaviour
         rigidbody.velocity = Vector3.Lerp(rigidbody.velocity, Vector3.Project(rigidbody.velocity, transform.forward), ForwardStabilization);
         rigidbody.AddForce(WindForce * transform.forward, ForceMode.Acceleration);
 
-        if (rigidbody.velocity.magnitude > currentSailMultiplier * CurrentMaxVelocity * (Boosting ? BoostMaxSpeedMultiplier : 1))
+        if (rigidbody.velocity.magnitude > CurrentMaxVelocity)
         {
-            rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, Mathf.Lerp(rigidbody.velocity.magnitude, currentSailMultiplier * CurrentMaxVelocity * (Boosting ? BoostMaxSpeedMultiplier : 1), VelocityLimitingStrength * Time.fixedDeltaTime));
+            rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, Mathf.Lerp(rigidbody.velocity.magnitude, CurrentMaxVelocity, VelocityLimitingStrength * Time.fixedDeltaTime));
         }
         rigidbody.AddTorque(TurningTorque * input.Turn * Vector3.up, ForceMode.Acceleration);
         rigidbody.AddTorque(TurningTorque * input.Pitch * Vector3.Cross(transform.forward, Vector3.up).normalized, ForceMode.Acceleration);
@@ -133,10 +79,5 @@ public class BoatMovement : MonoBehaviour
     void OnPitch(InputValue value)
     {
         input.Pitch = value.Get<float>();
-    }
-
-    void OnBoost(InputValue value)
-    {
-        input.Boost = Convert.ToBoolean(value.Get());
     }
 }
