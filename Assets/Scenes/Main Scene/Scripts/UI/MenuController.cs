3f1f87714dd64b02a149e3a447f96ec8529ea9ea
Manuel Guimaraes <manuel_guimaraes@hotmail.com>
2022-06-07 19:33:33 +0100

    Create a Main Scene folder with a Script and Scenes folder. Added a Main Menu Scene. Started Working on it, the idea is to have a very simple starting point to have a persistent game manager

---
+++ Assets/Scenes/Main Scene/Scripts/UI/MenuController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+    [Header("Levels to Load")]
+    public string _newGameLevel;
+    private string levelToLoad;
+
+    [SerializeField] private GameObject noSavedGameDialog = null;
+
+    // In case we want a prompt for a new game
+    public void NewGameDialogYes()
+    {
+        SceneManager.LoadScene(_newGameLevel);
+    }
+
+    public void LoadGameDialogYes()
+    {
+        if (PlayerPrefs.HasKey("SavedLevel"))
+        {
+            levelToLoad = PlayerPrefs.GetString("SavedLevel");
+            SceneManager.LoadScene(levelToLoad);
+        }
+        else
+        {
+            noSavedGameDialog.SetActive(true);
+        }
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
