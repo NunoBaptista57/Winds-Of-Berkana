d2d095fc8b4cad726bf88e1317de57ea18e7383f
DarthAkua <93348169+DarthAkua@users.noreply.github.com>
2023-10-22 20:19:50 +0100

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/TunelVento/MainTunelVento.cs
+++ Assets/Scripts/TunelVento/MainTunelVento.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using Cinemachine;
+using Vector3 = UnityEngine.Vector3;
 
 public class MainTunelVento : MonoBehaviour
 {
@@ -10,14 +12,21 @@ public class MainTunelVento : MonoBehaviour
 
     private bool overworldCamera = true;
 
+    public GameObject pointB;
+    public float rate;
+    
     [SerializeField]
     private CinemachineFreeLook playerCam;
     [SerializeField]
     private CinemachineFreeLook vcam2;
+    private Vector3 initialPlayerPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialPlayerPosition = _player.transform.position;
+        Vector3 position = pointB.transform.position;
+        //Vector3 position_corr = (position.x, position.y * 0.001, position.z);
     }
 
     // Update is called once per frame
@@ -32,6 +41,9 @@ public class MainTunelVento : MonoBehaviour
         if (target.gameObject.CompareTag("Player"))
         {
             SwitchPriority();
+            //target.gameObject.transform.position = Vector3.MoveTowards(target.gameObject.transform.position, pointB.transform.position, rate);
+            StartCoroutine(MovePlayerToPointB(target.gameObject));
+
         }
         
     }
@@ -58,5 +70,22 @@ public class MainTunelVento : MonoBehaviour
         }
 
         overworldCamera = !overworldCamera;
+    }
+    
+    IEnumerator MovePlayerToPointB(GameObject player)
+    {
+        float journeyLength = Vector3.Distance(player.transform.position, pointB.transform.position);
+        float journeyTime = journeyLength / rate;
+        float startTime = Time.time;
+        float distanceCovered = 0;
+
+        while (distanceCovered < journeyLength)
+        {
+            float distance = (Time.time - startTime) * rate;
+            float fractionOfJourney = distance / journeyLength;
+            player.transform.position = Vector3.Lerp(initialPlayerPosition, pointB.transform.position, fractionOfJourney);
+            distanceCovered = Vector3.Distance(player.transform.position, initialPlayerPosition);
+            yield return null;
+        }
     }
 }
