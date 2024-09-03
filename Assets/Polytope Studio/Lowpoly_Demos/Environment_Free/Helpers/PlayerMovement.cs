7d88045595b57de485f04f965a71be1a99ddac4d
Andre Correia <91163660+andremcorreia@users.noreply.github.com>
2024-09-03 19:33:52 +0100

    Merge and part 1 of gameplay- Art Merge

--- Assets/Polytope Studio/Lowpoly_Demos/Environment_Free/Helpers/PlayerMovement.cs
+++ Assets/Polytope Studio/Lowpoly_Demos/Environment_Free/Helpers/PlayerMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovementPoly : MonoBehaviour
 {
     public CharacterController controller;
 
